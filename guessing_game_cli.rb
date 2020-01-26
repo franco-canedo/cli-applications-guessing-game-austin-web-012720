e71f1e618c7fbf5a4672d00e9383038bd0cac22f
Franco Canedo <francanedo1@hotmail.com>
2020-01-26 21:41:59 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,7 +7,7 @@ def run_guessing_game
     puts "You guessed the correct number!"
   end 
   
-  if input != random_number || input != "exit"
+  if input != random_number && input != "exit"
     puts "Sorry! The computer guessed #{random_number}"
   end 
   
