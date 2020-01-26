91932e16681a20cf71b3e02d833212519d69b323
Franco Canedo <francanedo1@hotmail.com>
2020-01-26 21:55:48 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -8,7 +8,7 @@ def run_guessing_game
   if input.to_f == random_number
     print "You guessed the correct number!"
   else 
-    print "Sorry! The computer guessed #{random_number}"
+    print "Sorry! The computer guessed #{random_number}."
   end
   
   if input == "exit"
